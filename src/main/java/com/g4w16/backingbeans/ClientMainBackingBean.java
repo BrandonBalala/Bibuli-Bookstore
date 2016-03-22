aeb8be9eb8ca0bd26008b8e72ca317043c7ae437
1311213 <1311213@2F14-23.ad.dawsoncollege.qc.ca>
2016-03-22 12:24:43 -0400

    Commit of refactoring

---
+++ src/main/java/com/g4w16/backingbeans/ClientMainBackingBean.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.g4w16.backingbeans;
+
+/**
+ *
+ * @author 1311213
+ */
+public class ClientMainBackingBean {
+    
+}
---
+++ src/main/java/com/g4w16/backingbeans/LoginBackingBean.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.g4w16.backingbeans;
+
+import com.g4w16.entities.Client;
+import com.g4w16.jsf.controller.util.MessageUtil;
+import com.g4w16.persistence.ClientJpaController;
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.view.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author ofern
+ */
+@Named("loginBB")
+@ViewScoped
+public class LoginBackingBean implements Serializable {
+
+    @Inject
+    private ClientJpaController clientJPAController;
+
+    private String email;
+
+    private String password;
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+
+    }
+
+    /**
+     * Action
+     */
+    public void login() {
+
+        // Get Session object so that the status of the individual who just logged in
+        HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+
+        // Used to contain that will be displayed on the login form after Login button is pressed
+        FacesMessage message;
+        boolean loggedIn = false;
+
+        // Is there a client with these credentials
+        Client client = clientJPAController.findClientByEmailAndPassword(email, password);
+
+        // There is a client so login was successful
+        if (client != null) {
+            loggedIn = true;
+            message = MessageUtil.getMessage(
+                    "messages", "welcome", new Object[]{email});
+            message.setSeverity(FacesMessage.SEVERITY_INFO);
+        } else {
+            // Unsuccessful login
+            loggedIn = false;
+            // MessagesUtil simplifies creating localized messages
+            message = MessageUtil.getMessage(
+                    "messages", "loginerror", new Object[]{email});
+            message.setSeverity(FacesMessage.SEVERITY_ERROR);
+        }
+        // Store the outcome in the session object
+        session.setAttribute("loggedIn", loggedIn);
+
+        // Place the message in the context so that it will be displayed
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+}
