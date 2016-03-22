c1170a4c2890349069f23130ac29decf3239387b
1311213 <1311213@2F14-23.ad.dawsoncollege.qc.ca>
2016-03-22 13:57:17 -0400

    clientMainBacker

--- src/main/java/com/g4w16/backingbeans/ClientMainBackingBean.java
+++ src/main/java/com/g4w16/backingbeans/ClientMainBackingBean.java
@@ -5,10 +5,41 @@
  */
 package com.g4w16.backingbeans;
 
+import com.g4w16.entities.Books;
+import com.g4w16.persistence.BooksJpaController;
+import com.g4w16.persistence.PollJpaController;
+import java.io.Serializable;
+import java.util.List;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
 /**
  *
  * @author 1311213
  */
-public class ClientMainBackingBean {
+@Named("ClientMainBB")
+@SessionScoped
+public class ClientMainBackingBean implements Serializable{
     
+    @Inject
+    private BooksJpaController bookJpaController;
+    
+    @Inject
+    private PollJpaController pollJpaController;
+    
+    public List<Books> getBestSellers()
+    {
+       return  bookJpaController.findBestSellingBook(12);
+    }
+    
+    public List<Books> getRecentlyAdded()
+    {
+      return bookJpaController.findRecentlyAddedBooks(12);
+    }
+    
+    public void getBestSellersCount()
+    {
+       System.out.print(bookJpaController.findRecentlyAddedBooks(12).size());
+    }
 }
