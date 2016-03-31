a45cf5d2595f80f34e75f28a8749d9898122b91b
git <ofer.nitka-nakash@dawsoncollege.qc.ca>
2016-03-30 21:30:02 -0400

    made a client util class, will replace to use it everywhere by tomorrow evening.

---
+++ src/main/java/com/g4w16/backingbeans/ClientUtil.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.g4w16.backingbeans;
+
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author ofern
+ */
+public class ClientUtil {
+     private HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+    
+    public int getUserId()
+    {
+        return (int)session.getAttribute("client");
+    }
+    
+    public boolean isAuthenticated()
+    {
+        return (boolean)session.getAttribute("authenticated");
+    }
+}
