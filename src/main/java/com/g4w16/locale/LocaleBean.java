c4c8a919abc529a03399236a897f7a328cf8ab57
Paul Corrand <paul.corrand@hotmail.fr>
2016-04-05 10:03:31 -0400

    Deleted LocaleChanger, but kept its methods frenchAction and englishAction in the LocaleBean.

--- src/main/java/com/g4w16/locale/LocaleBean.java
+++ src/main/java/com/g4w16/locale/LocaleBean.java
@@ -1,9 +1,9 @@
 package com.g4w16.locale;
 
 import java.util.Locale;
-import javax.annotation.PostConstruct;
 import javax.inject.Named;
 import javax.faces.bean.SessionScoped;
+import javax.annotation.PostConstruct;
 import javax.faces.context.FacesContext;
 
 /**
@@ -33,6 +33,18 @@ public class LocaleBean {
     public void setLanguage(String language) {
         locale = new Locale(language);
         FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+    }
+
+	public String frenchAction() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.getViewRoot().setLocale(Locale.CANADA_FRENCH);
+        return null;
+    }
+
+    public String englishAction() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.getViewRoot().setLocale(Locale.CANADA);
+        return null;
     }
 
 }
