666f50d0f989f0dd09b0dd80c78772bf2b93df4d
git <ofer.nitka-nakash@dawsoncollege.qc.ca>
2016-04-05 20:05:08 -0400

    Working on files upload

--- src/main/java/com/g4w16/backingbeans/AdminBannerBackingBean.java
+++ src/main/java/com/g4w16/backingbeans/AdminBannerBackingBean.java
@@ -8,13 +8,18 @@ package com.g4w16.backingbeans;
 import com.g4w16.entities.Banner;
 import com.g4w16.persistence.BannerJpaController;
 import com.g4w16.persistence.exceptions.RollbackFailureException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.Serializable;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.RequestScoped;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.http.Part;
 import org.primefaces.event.RowEditEvent;
 
 /**
@@ -22,9 +27,10 @@ import org.primefaces.event.RowEditEvent;
  * @author wangdan
  */
 @Named("bannerBB")
-@RequestScoped
+@ViewScoped
 public class AdminBannerBackingBean implements Serializable {
 
+    private Part uploadedFile;
     private List<Banner> banners;
     private List<Integer> ids;
     private int bannerId;
@@ -39,6 +45,13 @@ public class AdminBannerBackingBean implements Serializable {
     public void init() {
         banners = bannerJpaController.findBannerEntities();
 
+    }
+      public Part getUploadedFile() {
+        return uploadedFile;
+    }
+
+    public void setUploadedFile(Part uploadedFile) {
+        this.uploadedFile = uploadedFile;
     }
 
     public List<Banner> getBanners() {
@@ -107,9 +120,12 @@ public class AdminBannerBackingBean implements Serializable {
         bannerJpaController.edit(selected);
     }
 
-    public void addAction(String uri) throws Exception {
-        Banner b=new Banner();
-        b.setUri(uri);
+    public void addAction(String name) throws Exception {
+        InputStream stream = uploadedFile.getInputStream();
+        Files.copy(stream, new File("/WEB-INF/books/"+name, name).toPath());
+        stream.close();
+        Banner b= new Banner();
+        b.setUri(name);
         b.setSelected(false);
         bannerJpaController.create(b);
         init();
--- src/main/java/com/g4w16/backingbeans/InvoiceBackingBean.java
+++ src/main/java/com/g4w16/backingbeans/InvoiceBackingBean.java
@@ -6,20 +6,16 @@
 package com.g4w16.backingbeans;
 
 import com.g4w16.entities.Sales;
-import com.g4w16.jsf.util.ResponseCatcher;
 import com.g4w16.persistence.SalesJpaController;
 import java.io.IOException;
 import java.io.Serializable;
 import java.text.DecimalFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
 import javax.enterprise.context.SessionScoped;
-import javax.faces.application.FacesMessage;
-import javax.faces.application.ViewHandler;
-import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -49,41 +45,13 @@ public class InvoiceBackingBean implements Serializable {
     public void sendInvoice(Sales sale)
     {
         setSale(sale);
-        this.email = renderView("invoice");
+        
     }
     
     public String getEmail()
     {
         return email;
     }
-    /**
-     * https://developer.jboss.org/message/580938#580938
-     * @param template
-     * @return 
-     */
-    public String renderView(String template) {
-            FacesContext faces = FacesContext.getCurrentInstance();
-            ExternalContext context = faces.getExternalContext();
-           
-            HttpServletResponse response = (HttpServletResponse)
-                    context.getResponse();
-            ResponseCatcher catcher = new ResponseCatcher(response);
-            try {
-            ViewHandler views = faces.getApplication().getViewHandler();
-            // render the message
-                context.setResponse(catcher);
-                context.getRequestMap().put("emailClient", true);
-                views.renderView(faces, views.createView(faces, template));
-                context.getRequestMap().remove("emailClient");
-                context.setResponse(response);
-            } catch (IOException ioe) {
-                String msg = "Failed to render email internally";
-                faces.addMessage(null, new FacesMessage(
-                        FacesMessage.SEVERITY_ERROR, msg, msg));
-                return null;
-            }
-            return catcher.toString();
-        }
     
     public void sendToInvoicesList() throws IOException {
          if (sale == null)
